0a6f3f5dae0735324e7fcb3cbfaa89864f18ddad
Mbilal6799 <ftk4321@gmail.com>
2023-07-21 14:13:09 -0700

    Login Ui, SignUpUI and Routing done

--- ecomerce_react_app/src/App.js
+++ ecomerce_react_app/src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import logo from "./logo.svg";
 import Home from "./pages/Home";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+]);
 export default function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
